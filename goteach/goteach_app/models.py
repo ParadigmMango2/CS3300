48158076189917c94a365d5d2d45d55606140336
ParadigmMango2 <jacobjhartt@gmail.com>
2024-04-06 10:59:25 -0600

    Class model implemented

--- goteach/goteach_app/models.py
+++ goteach/goteach_app/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
+class Class(models.Model):
+	title = models.CharField(max_length=200)
+	ended = models.BooleanField()
+	about = models.CharField(max_length=5000, blank = True)
+	game_link = models.CharField(max_length=500, blank = True)
+
+	# Override the string/name function
+	def __str__(self):
+		return self.title
+
+	# Returns the absolute url
+	#def get_absolute_url(self):
+	# 	return reverse('portfolio_detail', args=[str(self.id)])
+
