e615a0a6663bc3383c6981a47c2a25c76b7167a1
ParadigmMango2 <jacobjhartt@gmail.com>
2024-04-17 11:08:29 -0600

    Media folder set up

--- goteach/goteach_app/urls.py
+++ goteach/goteach_app/urls.py
@@ -1,6 +1,9 @@
 # Author: Jacob Hartt
 
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
+
 from . import views
 
 from .views import *
@@ -15,3 +18,6 @@ urlpatterns = [
 	path('classes/<int:class_id>/delete_class/', views.deleteClass, name='delete_class'), 
 ]
 
+if settings.DEBUG:
+	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+	
