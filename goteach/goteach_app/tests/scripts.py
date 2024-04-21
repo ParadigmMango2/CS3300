dcd1157b70cd41887a2115c87c9caf07236b1801
ParadigmMango2 <jacobjhartt@gmail.com>
2024-04-20 23:49:01 -0600

    Moved login script to a seperate file so it can be used by various other files

--- goteach/goteach_app/tests/scripts.py
+++ goteach/goteach_app/tests/scripts.py
@@ -1,29 +1,37 @@
-# create_session_cookie.py
-from django.conf import settings
-from django.contrib.auth import (
-    SESSION_KEY, BACKEND_SESSION_KEY, HASH_SESSION_KEY,
-    get_user_model
-)
-from django.contrib.sessions.backends.db import SessionStore
+from django.contrib.auth import get_user_model
+from django.contrib.auth.models import Group
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
-def create_session_cookie(username, password):
 
-    # First, create a new test user
+
+def login_as_teacher(driver, live_server_url):
+    test_username = 'TestTeacher'
+    test_pwd = 'very=seKret23'
+
+    # Create user
     User = get_user_model()
-    user = User.objects.create_user(username=username, password=password)
+    # try:
+    # 	User.objects.create_user(username=test_username, password=test_pwd)
+    # except Exception as e:
+    # 	print(f"Error creating user: {e}")
+    user = User.objects.create_user(username=test_username, password=test_pwd)
 
-    # Then create the authenticated session using the new user credentials
-    session = SessionStore()
-    session[SESSION_KEY] = user.pk
-    session[BACKEND_SESSION_KEY] = settings.AUTHENTICATION_BACKENDS[0]
-    session[HASH_SESSION_KEY] = user.get_session_auth_hash()
-    session.save()
+    # Create teachers group and add user to it
+    teachers_group = Group.objects.get_or_create(name='Teachers')
+    teachers_group_id = Group.objects.get(name = 'Teachers')
+    user.groups.add(teachers_group_id)
 
-    # Finally, create the cookie dictionary
-    cookie = {
-        'name': settings.SESSION_COOKIE_NAME,
-        'value': session.session_key,
-        'secure': False,
-        'path': '/',
-    }
-    return cookie
+    # driver.get('http://127.0.0.1:8000/')                      
+    driver.get(live_server_url)
+
+    # Navigate to login page
+    driver.find_element(By.ID, "login-nav-button").click()          
+    assert "/login/" in driver.current_url 
+
+    # Login
+    driver.find_element(By.ID, "id_username").send_keys(test_username)          
+    driver.find_element(By.ID, "id_password").send_keys(test_pwd)          
+    driver.find_element(By.ID, "login-button").click()          
