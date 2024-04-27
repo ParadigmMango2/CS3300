226ceb7d60be1ff0411e7c004d9150a7fedf88f2
ParadigmMango2 <jacobjhartt@gmail.com>
2024-04-26 21:10:48 -0600

    Added class instance test

--- goteach/goteach_project/settings.py
+++ goteach/goteach_project/settings.py
@@ -129,12 +129,12 @@ STATIC_URL = 'static/'
 
 
 # Set Static file root based on debug status
-if DEBUG:
-    STATIC_ROOT = BASE_DIR / "static/"
-else:
-    STATICFILES_DIRS = [
-        os.path.join(BASE_DIR, 'static/'),
-    ]
+# if DEBUG:
+STATIC_ROOT = BASE_DIR / "static/"
+# else:
+#     STATICFILES_DIRS = [
+#         os.path.join(BASE_DIR, 'static/'),
+#     ]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
